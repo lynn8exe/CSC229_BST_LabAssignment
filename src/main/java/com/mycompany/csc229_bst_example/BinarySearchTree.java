c316278416fa9185aafc968cab82798d6720b540
Adrianna Lambert <153338615+lynn8exe@users.noreply.github.com>
2025-11-26 12:27:50 -0500

    DONE

--- src/main/java/com/mycompany/csc229_bst_example/BinarySearchTree.java
+++ src/main/java/com/mycompany/csc229_bst_example/BinarySearchTree.java
@@ -1,10 +1,11 @@
 package com.mycompany.csc229_bst_example;
 /**
  *
- * @author MoaathAlrajab
+ * @author AdriannaLambert
  */
 import java.util.LinkedList;
 import java.util.Queue;
+
 
 public class BinarySearchTree {
     private BstNode root;
@@ -55,30 +56,84 @@ public class BinarySearchTree {
     }
 
     private void doInOrder(BstNode root) {
+    // ToDo 1: complete InOrder Traversal CHECK
 
-        // ToDo 1: complete InOrder Traversal 
+        if (root == null) {
+            return;
+        }
+        doInOrder(root.getLeft());
+        System.out.print(root.getData() + " ");
+        doInOrder(root.getRight());
     }
-        public void preOrderTraversal() {
+
+
+    public void preOrderTraversal() {
         doPreOrder(this.root);
-        // ToDo 2: complete the pre-order travesal . 
+        // ToDo 2: complete the pre-order travesal .
+    }
+
+    // You need to create this separate private method:
+    private void doPreOrder(BstNode root) {
+        if (root == null) return;
+
+        System.out.print(root.getData() + " ");
+        doPreOrder(root.getLeft());
+        doPreOrder(root.getRight());
     }
 
     public Integer findHeight() {
-
         // ToDo 3: Find the height of a tree
+        return findHeight(this.root);
     }
 
-    
+    private Integer findHeight(BstNode node) {
+        if (node == null) {
+            return -1;
+        }
+        int leftHeight = findHeight(node.getLeft());
+        int rightHeight = findHeight(node.getRight());
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
 
     public int getDepth(BstNode node) {
-        //ToDo 4: complete getDepth of a node 
+        //ToDo 4: complete getDepth of a node
+        int depth = 0;
+        BstNode current = this.root;
+
+        while (current != null) {
+            // if node is found return depth
+            if (node.getData().equals(current.getData())) {
+                return depth;
+            }
+
+            //smaller - Left, bigger - Right.
+            if (node.getData() < current.getData()) {
+                current = current.getLeft();
+            } else {
+                current = current.getRight();
+            }
+
+            depth++;
+        }
+        return -1;
     }
-    
-   public void print() {
-       System.out.println("\n==== BST Print ===== \n");
+
+    public void print() {
+        System.out.println("\n==== BST Print ===== \n");
         print("", root, false);
         // ToDo 5: complete the print of the BST
     }
 
+    private void print(String prefix, BstNode node, boolean isLeft) {
+        if (node != null) {
+
+            System.out.println(prefix + (isLeft ? "|-- " : "\\-- ") + node.getData());
+            String nextPrefix = prefix + (isLeft ? "|   " : "    ");
+
+            print(nextPrefix, node.getLeft(), true);
+            print(nextPrefix, node.getRight(), false);
+        }
+    }
 
 }
